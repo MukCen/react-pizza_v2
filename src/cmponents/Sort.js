7f100b14e3b8170b686211199893dbfb6ba535f2
MukCen <mukola_1973@ukr.net>
2023-12-14 20:06:05 +0200

    Sort Popup

--- src/cmponents/Sort.js
+++ src/cmponents/Sort.js
@@ -1,31 +1,73 @@
+import { useEffect, useRef, useState } from "react";
+
 function Sort() {
+  const sortRef = useRef(null);
+  console.log(sortRef);
+  const [visiblePopap, setVisiblePopap] = useState(false);
+  const [popapAcktive, setPopapAcktive] = useState(0);
+
+  const sortPopaps = ["популярности", "цене", "алфавиту"];
+
+  const handlePopapAcktive = (ind) => {
+    setPopapAcktive(ind);
+    setVisiblePopap(false);
+  };
+
+  // useEffect(() => {
+  //   const eventsPopap = (event) => {
+  //     const _event = event || {};
+  //     if (
+  //       sortRef.current &&
+  //       (!_event.path || !_event.path.includes(sortRef.current))
+  //     ) {
+  //       // console.log(sortRef.current);
+  //       // console.log(_event.path);
+  //       setVisiblePopap(false);
+  //     }
+  //   };
+
+  //   document.body.addEventListener("click", eventsPopap);
+  //   return () => document.body.removeEventListener("click", eventsPopap);
+  // }, []);
+
   return (
-    <div className='sort'>
+    <div
+      ref={sortRef}
+      className='sort'>
       <div className='sort__label'>
         <svg
           width='10'
           height='6'
           viewBox='0 0 10 6'
           fill='none'
-          xmlns='http://www.w3.org/2000/svg'
-        >
+          xmlns='http://www.w3.org/2000/svg'>
           <path
             d='M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z'
             fill='#2C2C2C'
           />
         </svg>
         <b>Сортировка по:</b>
-        <span>популярности</span>
+        <span onClick={() => setVisiblePopap(!visiblePopap)}>
+          {sortPopaps[popapAcktive]}
+        </span>
       </div>
-      <div className='sort__popup'>
-        <ul>
-          <li className='active'>популярности</li>
-          <li>цене</li>
-          <li>алфавиту</li>
-        </ul>
-      </div>
+      {visiblePopap ? (
+        <div className='sort__popup'>
+          <ul>
+            {sortPopaps.map((popapName, i) => (
+              <li
+                key={i}
+                onClick={() => handlePopapAcktive(i)}
+                className={popapAcktive === i ? "active" : ""}>
+                {popapName}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
-
 export default Sort;
